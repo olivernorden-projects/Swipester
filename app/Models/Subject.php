68df6ee02c544e9925540d4b5a67b1944331c0fd
Oliver Nordén <oliver@kreationsbyran.se>
2021-04-19 21:12:35 +0200

    feat(subject): has many relation to items

--- app/Models/Subject.php
+++ app/Models/Subject.php
@@ -2,10 +2,15 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\SubjectItem;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Subject extends Model
 {
     use HasFactory;
+
+    public function items() {
+        return $this->hasMany(SubjectItem::class);
+    }
 }
