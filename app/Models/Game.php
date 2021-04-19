79dd9852268e380b29d7bc9e20c0193ce1b76a82
Oliver Nordén <oliver@kreationsbyran.se>
2021-04-19 22:23:12 +0200

    feat(game): add belongs to subject

--- app/Models/Game.php
+++ app/Models/Game.php
@@ -2,10 +2,15 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Subject;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Game extends Model
 {
     use HasFactory;
+
+    public function subject() {
+        return $this->belongsTo(Subject::class);
+    }
 }
