58a9ceec97f17a4c6393932a9acfd76f76577c7d
Oliver Nordén <oliver@kreationsbyran.se>
2021-04-20 10:50:37 +0200

    feat(client): add index page with description

--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -6,4 +6,13 @@
 </template>
 
 <style>
+  h1 {
+    text-align: center;
+    margin-top: 2rem;
+  }
+  p {
+    text-align: center;
+    max-width: 400px;
+    margin: auto;
+  }
 </style>
--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,7 +1,8 @@
 <template>
-  <div class="container">
+  <b-container>
     <h1>Swipester</h1>
-  </div>
+    <p>Swipester is a game where two people are shown various items realated to a given subject, such as movies or food, and asked to either like or dislike the item to find items with common approval.</p>
+  </b-container>
 </template>
 
 <script>
