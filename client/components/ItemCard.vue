725c6512a32bb68f91944ac8b2dceb0cdda86e81
Oliver Nordén <oliver@kreationsbyran.se>
2021-04-20 12:57:22 +0200

    feat(item card): add item card

---
+++ client/components/ItemCard.vue
@@ -0,0 +1,45 @@
+<template>
+  <b-card
+    :title="item.title"
+    :img-src="item.image"
+    :img-alt="item.title"
+    img-top
+    tag="article"
+    style="max-width: 20rem;"
+    class="mb-2"
+  >
+    <b-card-text v-if="item.description">
+      {{ item.description }}
+    </b-card-text>
+
+    <b-button variant="outline-danger" @click="$emit('Dislike', item.id)">
+      Dislike
+    </b-button>
+    <b-button variant="outline-success" @click="$emit('Like', item.id)">
+      Like
+    </b-button>
+  </b-card>
+</template>
+
+<script>
+export default {
+  props: {
+    item: {
+      type: Object,
+      default: () => ({
+        id: 0,
+        title: '',
+        description: null,
+        image: null
+      })
+    }
+  }
+}
+</script>
+
+<style>
+  .card {
+    margin: 2rem auto 0;
+    text-align: center;
+  }
+</style>
--- client/pages/game/_hash.vue
+++ client/pages/game/_hash.vue
@@ -1,9 +1,17 @@
 <template>
-  <h1>{{ game.subject.title }}</h1>
+  <b-container>
+    <h1>{{ game.subject.title }}</h1>
+    <ItemCard :item="nextItem" @Like="Like" @Dislike="Dislike" />
+  </b-container>
 </template>
 
 <script>
+import ItemCard from '../../components/ItemCard'
+
 export default {
+  components: {
+    ItemCard
+  },
   async asyncData ({ error, params, $axios }) {
     const { hash } = params
     try {
@@ -11,6 +19,19 @@ export default {
       return { game }
     } catch ({ response }) {
       error({ statusCode: response.status, message: response.data.message })
+    }
+  },
+  computed: {
+    nextItem () {
+      return this.game.subject.items[0]
+    }
+  },
+  methods: {
+    Like () {
+      alert('like')
+    },
+    Dislike () {
+      alert('dislike')
     }
   }
 }
