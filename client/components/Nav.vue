5aa2de1075bae09d89c458de66de839424d86c78
Oliver Nordén <oliver@kreationsbyran.se>
2021-04-20 10:41:30 +0200

    feat(nav): add navbar

---
+++ client/components/Nav.vue
@@ -0,0 +1,22 @@
+<template>
+  <b-navbar toggleable="lg" type="dark" variant="dark">
+    <b-navbar-brand to="/">
+      Swipester
+    </b-navbar-brand>
+    <b-navbar-nav class="ml-auto">
+      <b-nav-item href="https://olivernorden.se" rel="noopener">
+        olivernorden.se
+      </b-nav-item>
+    </b-navbar-nav>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
