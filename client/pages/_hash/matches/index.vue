19e828125882d12d88544f6cf03d68766614b46c
Oliver Nordén <oliver@kreationsbyran.se>
2021-04-20 18:45:06 +0200

    feat(matches page): fetch swipes

--- client/pages/_hash/matches/index.vue
+++ client/pages/_hash/matches/index.vue
@@ -1,10 +1,30 @@
 <template>
-  <h1>Matches</h1>
+  <div>
+    <h1>Matches</h1>
+    <p>
+      <nuxt-link :to="matchesLink">
+        Continue swiping
+      </nuxt-link>
+    </p>
+  </div>
 </template>
 
 <script>
 export default {
-
+  async asyncData ({ error, params, $axios }) {
+    const { hash } = params
+    try {
+      const { data: swipes } = await $axios.get(`/api/game/${hash}/swipe`)
+      return { swipes }
+    } catch ({ response }) {
+      error({ statusCode: response.status, message: response.data.message })
+    }
+  },
+  computed: {
+    matchesLink () {
+      return `/${this.$route.params.hash}`
+    }
+  }
 }
 </script>
 
