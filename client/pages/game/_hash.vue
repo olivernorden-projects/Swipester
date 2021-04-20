771cea6b05013e4cc28d339a0ee38b422c4f38ea
Oliver Nordén <oliver@kreationsbyran.se>
2021-04-20 12:19:33 +0200

    feat(game): fetch game data based on player hash

--- client/pages/game/_hash.vue
+++ client/pages/game/_hash.vue
@@ -1,10 +1,18 @@
 <template>
-  <h1>Game: {{ $route.params.hash }}</h1>
+  <h1>{{ game.subject.title }}</h1>
 </template>
 
 <script>
 export default {
-
+  async asyncData ({ error, params, $axios }) {
+    const { hash } = params
+    try {
+      const { data: game } = await $axios.get(`/api/game/${hash}`)
+      return { game }
+    } catch (e) {
+      error({ statusCode: 404, message: 'Game not found' })
+    }
+  }
 }
 </script>
 
