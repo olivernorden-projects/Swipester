077249c816e67842f206d3ccb1637bc95dda8b7c
Oliver Nordén <oliver@kreationsbyran.se>
2021-04-20 11:29:10 +0200

    feat(index): add game selection buttona

---
+++ client/components/GameSelector.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="game-selector__container">
+    <b-button v-for="subject in availableGames" :key="subject.id" variant="outline-primary" class="game-selector__btn">
+      {{ subject.title }}
+    </b-button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    availableGames: {
+      type: Array,
+      default: () => ([])
+    }
+  }
+}
+</script>
+
+<style>
+  .game-selector__container {
+    display: flex;
+    justify-content: center;
+    margin-top: 1rem;
+  }
+  .game-selector__btn {
+    margin: 0 .5rem;
+  }
+</style>
--- client/pages/index.vue
+++ client/pages/index.vue
@@ -2,9 +2,23 @@
   <b-container>
     <h1>Swipester</h1>
     <p>Swipester is a game where two people are shown various items realated to a given subject, such as movies or food, and asked to either like or dislike the item to find items with common approval.</p>
+
+    <GameSelector :available-games="availableGames" />
   </b-container>
 </template>
 
 <script>
-export default {}
+import GameSelector from '../components/GameSelector'
+
+export default {
+  components: {
+    GameSelector
+  },
+  async asyncData ({ $axios }) {
+    const { data: availableGames } = await $axios.get('/api/game')
+    return {
+      availableGames
+    }
+  }
+}
 </script>
