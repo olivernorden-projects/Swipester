6f86999f979e8b0f2ba2d1d9353acc1e4a29bf6e
Oliver Nordén <oliver@kreationsbyran.se>
2021-04-20 11:45:44 +0200

    feat(game selector): create game and redirect to game page

--- client/components/GameSelector.vue
+++ client/components/GameSelector.vue
@@ -1,17 +1,26 @@
 <template>
   <div class="game-selector__container">
-    <b-button v-for="subject in availableGames" :key="subject.id" variant="outline-primary" class="game-selector__btn">
+    <b-button v-for="subject in availableGames" :key="subject.id" variant="outline-primary" class="game-selector__btn" @click="createGame(subject.id)">
       {{ subject.title }}
     </b-button>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   props: {
     availableGames: {
       type: Array,
       default: () => ([])
+    }
+  },
+  methods: {
+    async createGame (subjectId) {
+      const { data: game } = await axios.post(`/api/game/${subjectId}`)
+      const playerHash = game.players[0].hash
+      this.$router.push(`/game/${playerHash}`)
     }
   }
 }
